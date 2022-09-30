31fe81c2931620b0b91cf3c96e065dce56dacfb4
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-09-29 22:28:51 -0300

    plusMinus challenge

---
+++ plusMinus.js
@@ -0,0 +1,35 @@
+
+
+
+
+/* tamanho do array: maior que 0 e menor que 100
+    verificar quantidade de positivos, negativos e 0's e armazenar em variaveis as qtdes 
+    o resultado deve ser impresso com 6 casas decimais*/
+
+let arr = [1, 2, 3, 0, -2, -4]
+
+function plusMinus(arr) {
+
+    if(arr.length > 100 || arr.length <= 0) return;
+
+    let arrSize = arr.length;
+    let positives = 0;
+    let negatives = 0;
+    let zeros = 0;
+
+    arr.forEach(number => {
+        if(number < -100 || number > 100) return;
+
+        if(number == 0) {
+            zeros ++;
+        }else if (number > 0){
+            positives++;
+        } else{
+            negatives++;
+        }
+    })
+
+    console.log((zeros/arrSize).toFixed(6) + "\n" + (positives/arrSize).toFixed(6) + "\n" + (negatives/arrSize).toFixed(6));
+}
+
+plusMinus(arr);
