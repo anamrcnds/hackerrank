797bf0c239d651fbb30569330482bbc18c14d4a5
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-09-29 22:28:22 -0300

    camelCase challenge

---
+++ camelCase.js
@@ -0,0 +1,16 @@
+function camelcase(s) {
+    if(s.length < 1 || s.length > 100000) return;
+    
+    let characters = '[A-Z]';
+    let words = 1;
+
+    for(let letra of s){
+        if(letra.match(characters)){
+            words++;
+        }
+    }
+
+    console.log(words)
+}
+
+camelcase('euMeChamoAnaEVoce')
